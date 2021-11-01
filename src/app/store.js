e370906f64ffdf3383873490fb304f8220114dec
Mayahi Adel <adel.mayahii@gmail.com>
2021-11-01 21:24:31 +0100

    build: home page

---
+++ src/app/store.js
@@ -0,0 +1,8 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { cryptoApi } from "../sevices/cryptoApi";
+
+export default configureStore({
+  reducer: {
+    [cryptoApi.reducerPath]: cryptoApi.reducer,
+  },
+});
---
+++ src/sevices/cryptoApi.js
@@ -0,0 +1,22 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const cryptoApiHeaders = {
+  "x-rapidapi-host": "coinranking1.p.rapidapi.com",
+  "x-rapidapi-key": "5e8fc764b9msh8f14ff4e80804b8p16716ajsn939f5933590f",
+};
+
+const baseUrl = "https://coinranking1.p.rapidapi.com";
+
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
+
+export const cryptoApi = createApi({
+  reducerPath: "cryptoApi",
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    getCryptos: builder.query({
+      query: () => createRequest("/coins"),
+    }),
+  }),
+});
+
+export const { useGetCryptosQuery } = cryptoApi;
